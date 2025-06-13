06c537ca24675a1a1ec0fbd742f4797d13a4bfa4
renjur2003 <renju.stackuponline@gmail.com>
2025-06-13 11:59:01 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+// imports 
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const session=require ('express-session');
+
+const app = express();
+const port = process.env.port || 4000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(session({secret:"my secret key",
+    saveUninitialized:true,
+    resave:false,
+}));
+
+app.use((req, res, next) => {
+    res.locals.message = req.session.message || null;
+    delete req.session.message;
+    next();
+});
+
+app.use('/uploads', express.static('uploads'));
+
+
+
+//set template engine
+app.set("view engine", "ejs");
+
+
+// route prefix 
+app.use("",require("./routes/routes")); 
+
+
+app.listen( port,() => {
+    console.log(`server started at http://localhost:${port}`);
+    
+});
